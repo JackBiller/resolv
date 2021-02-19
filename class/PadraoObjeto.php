f74ef1579a80a1e90ebae10273050018589e4085
Jack Biller <jackbiller19@gmail.com>
2021-02-18 22:30:48 -0300

    Change PadraoObjeto

--- class/PadraoObjeto.php
+++ class/PadraoObjeto.php
@@ -1,36 +1,36 @@
 <?php
 
-class PadraoObjeto {
+class PadraoObjeto { 
 	var $debug = 'OK';
-	public function get($nome_campo){
+	public function get($nome_campo) { 
 		return $this->$nome_campo;
 	}
 
-	public function set($valor , $nome_campo){
+	public function set($valor , $nome_campo) { 
 		$this->$nome_campo = $valor;
 	}
 
-	public function check($nome_campo){
+	public function check($nome_campo) { 
 		return isset($this->$nome_campo);
 	}
 
-	public function push($valor, $nome_campo){
-		if(gettype($this->$nome_campo) == "array") array_push($this->$nome_campo, $valor);
+	public function push($valor, $nome_campo) { 
+		if (gettype($this->$nome_campo) == "array") array_push($this->$nome_campo, $valor);
 	}
 
-	public function removeQuebra($tipo, $valor){
+	public function removeQuebra($tipo, $valor) { 
 							$valor = 	str_replace("\"", '\'',
 										str_replace("\r", '', $valor));
-		if($tipo == 'html') return 		str_replace("\t", '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+		if ($tipo == 'html') return 		str_replace("\t", '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
 										str_replace("\n", '<br>', $valor));
 		else 				return 		str_replace("\t", ' ',
 										str_replace("\n", '', $valor));
 	}
 }
 
-class FalseDebug extends PadraoObjeto{ 
-	public function __construct($msm){
-		if(!empty($msm) && gettype($msm) == 'string') $this->set($msm, 'debug');
+class FalseDebug extends PadraoObjeto { 
+	public function __construct($msm) { 
+		if (!empty($msm) && gettype($msm) == 'string') $this->set($msm, 'debug');
 	}
 }
 
