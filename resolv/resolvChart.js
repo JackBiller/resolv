028735e2f43c3ae2495912e76839fd5f66872f1c
jackbiller19 <jackbiller19@gmail.com>
2021-07-29 13:02:16 -0300

    Update resolvChart.js

--- resolv/resolvChart.js
+++ resolv/resolvChart.js
@@ -1,7 +1,7 @@
 
 var registerChart_Global = [];
 
-var styleGenerico_Global = { 
+var styleGenerico_Global = {
 	alternarColor: '',
 	numberFormat: '#',
 	tamanhoFixo: true,
@@ -12,7 +12,7 @@ var styleGenerico_Global = {
 }
 
 
-function resolvChart(data, options) { 
+function resolvChart(data, options) {
 	/*
 		styleGenerico = {
 			alternarColor: (0|1) 					-- Colore o fundo do grafico cor sim e cor não
@@ -21,53 +21,53 @@ function resolvChart(data, options) {
 			legendaChart: (0|1) 					-- Se vai ter ou não
 			separadorDecimal: ',' 					-- Texto para separador decimal
 			separadorMilhar: '.' 					-- Texto para separador de milhar
-			linhaPontilhada: '5.5' 					-- Parametro de pontilhado da linha
+			linhaPontilhada: '5.5' 					-- Parâmetro de pontilhado da linha
 		}
 
 		options: {
-			category: { 							-- Parametro que indica a categoria do grafico
-				text: ''							-- Texto do parametro
-				param: ''							-- Parametro do objeto data correspondente
-				click: function(index){}			-- Click considerando a categoria inteira, passar como parametro o indice da categoria / dado
+			category: { 							-- Parâmetro que indica a categoria do grafico
+				text: ''							-- Texto do parâmetro
+				param: ''							-- Parâmetro do objeto data correspondente
+				click: function(index){}			-- Click considerando a categoria inteira, passar como parâmetro o índice da categoria / dado
 				rotation: num/graus					-- Graus de rotação da descrição da categoria
-				textUpDown: (0|1) 					-- Descrição, no eixo da catergorias, colocando um para cima e outro para baixo
+				textUpDown: (0|1) 					-- Descrição, no eixo da categorias, colocando um para cima e outro para baixo
 			}
-			value: [] / { 							-- Parametro que indica o valor do grafico / Pode ser um objeto ou array de objetos
-				text: ''							-- Texto do parametro
-				min: num 							-- O valor minimo que começa o gráfico, geramente fica para a lib definir automatico
-				max: num 							-- O valor máximo que pode chegar o gráfico, geramente fica para a lib definir automatico
+			value: [] / { 							-- Parâmetro que indica o valor do grafico / Pode ser um objeto ou array de objetos
+				text: ''							-- Texto do parâmetro
+				min: num 							-- O valor minimo que começa o gráfico, geralmente fica para a lib definir automático
+				max: num 							-- O valor máximo que pode chegar o gráfico, geralmente fica para a lib definir automático
 				opposite: (0|1) 					-- Se vai desenhar a escala do lado oposto, padrão false
 				syncWithAxis: index 				-- Se possui mais de uma escala e precisa sincronizar o valor com outra escala
-				... 								-- valor é o indice da outra escala
+				... 								-- valor é o índice da outra escala
 			}
 			serie: [] / { 							-- Pode ser apenas um objeto ou um array de objetos
 				type: (column|line)  				-- Tipo de serie se vai montar grafico de linha ou coluna, padrão é column
-				param: ''							-- Parametro do objeto data correspondente
+				param: ''							-- Parâmetro do objeto data correspondente
 				click: function(){} 				-- Quando clicar no gráfico
 				name: '' 							-- Nome do gráfico
 				color: 'lightblue' 					-- Cor que vai ter no grafico
-				onlyColumn: (0|1) 					-- Deixar uma coluna emcima da outra, padrão vardadeiro
+				onlyColumn: (0|1) 					-- Deixar uma coluna em cima da outra, padrão verdadeiro
 				label: (0|1) / { 					-- Caso for definido como false não irá mostrar no gráfico
-					color: 'black' 					-- Cor da font da label que é motrar o valor no grafico
-					align: (center|in|out)			-- Alinhamento da label se é centrelizado dentro ou fora o padrão é 'out'
+					color: 'black' 					-- Cor da font da label que é mostrar o valor no grafico
+					align: (center|in|out)			-- Alinhamento da label se é centralizado dentro ou fora o padrão é 'out'
 					mask: '%0%' 					-- Mascara da label
 				}
 				ball: (0|1) / { 					-- Caso gráfico de linha sem click na serie irá desenhar somente as bolas em cada categoria
 					color: '' 						-- Preenchimento da cor da bolinha
 				}
-				pontilhado: (0|1) 					-- Caso gráfico de linha denhar a linha pontilhada ou reta
+				pontilhado: (0|1) 					-- Caso gráfico de linha desenhar a linha pontilhada ou reta
 				tooltip: '' 						-- Texto em HTML
 				value: index 						-- Se possui mais de uma escala precisa informar qual escala é referente a essa serie
-				... 								-- valor é o indice da escala
+				... 								-- valor é o índice da escala
 				hide: (0|1) 						-- Indicar se vai ocultar no gráfico, padrão false, atualizado pre e pos-render
 			}
-			descForm: '' 							-- Parametro de identificação
+			descForm: '' 							-- Parâmetro de identificação
 			padination: num 						-- Trazer tantos valores pré definidos na tela
 			orientation: 'horizontal' 				-- Orientação do grafico, (horizontal | vertical)
 			percentual: (0|1) 						-- Se o grafico vai mostrar valor percentual de 0 a 100
 			width: '100%' 							-- Largura do grafico
 			height: '400px' 						-- Altura do gráfico
-			styleGenerico: {} 						-- Configurações genericas para todos os grafico gerados apartir
+			styleGenerico: {} 						-- Configurações genéricas para todos os grafico gerados a partir
 			setLegend: (0|1) 						-- Se vai mostrar legenda do grafico
 			legend: { 								-- Configurações de legenda
 				position: 'bottom|top|right|left' 	-- Posição onde vai ficar a legenda (padrão bottom)
@@ -84,7 +84,7 @@ function resolvChart(data, options) {
 
 	if (
 		(options.forceSet || '') != '' && registerChart_Global.indexOf(options.descForm) >= 0
-	) { 
+	) {
 		registerChart_Global.splice(registerChart_Global.indexOf(options.descForm),1);
 	}
 
@@ -93,12 +93,12 @@ function resolvChart(data, options) {
 	// 	random = parseInt( Math.random() * 100000 );
 	// } while (registerChart_Global.indexOf(random) != -1);
 
-	data.forEach(function(dt,i) { 
+	data.forEach(function(dt,i) {
 		data[i].lineDash = styleGenerico.linhaPontilhada;
 	});
 
-	// Verificar se o parametro é um objeto unitario e passar para array
-	['serie','value'].forEach(function(e) { 
+	// Verificar se o parâmetro é um objeto unitário e passar para array
+	['serie','value'].forEach(function(e) {
 		// options.serie, options.value
 		var keys = Object.keys((options[e] || {}));
 		if (keys.length > 0 && isNaN(keys[0])) 	options[e] = [options[e]];
@@ -114,16 +114,16 @@ function resolvChart(data, options) {
 
 		window["chart"+options.descForm].data = data;
 
-		if ((options.padination || '') != '' && !isNaN(options.padination)) { 
+		if ((options.padination || '') != '' && !isNaN(options.padination)) {
 			window["categoryAxis"+options.descForm].start 	= 0;
 			window["categoryAxis"+options.descForm].end 	= data.length < parseInt(options.padination) ? 1 : parseInt(options.padination) / data.length;
 		}
 
-		options.serie.forEach(function(s,i) { 
+		options.serie.forEach(function(s,i) {
 			window["series"+i+options.descForm][((s.hide || '') == '' ? 'show' : 'hide')]();
 		});
 
-		if ((options.title || '') != '') { 
+		if ((options.title || '') != '') {
 			window["titleChart"+options.descForm].text = options.title;
 		}
 
@@ -131,7 +131,7 @@ function resolvChart(data, options) {
 	}
 
 
-	var setLegend = false; // variavel para mostrar legenda
+	var setLegend = false; // variável para mostrar legenda
 
 
 	if ((options.orientation || '') == '') options.orientation = 'horizontal';
@@ -174,7 +174,7 @@ function resolvChart(data, options) {
 			+t(2) 	+ 		`<input type="checkbox" checked`
 
 					+ ` onclick='`
-					+ 	options.serie.map(function(e,i) { 
+					+ 	options.serie.map(function(e,i) {
 							return `series${i+options.descForm}._tooltip.disabled = !series${i+options.descForm}._tooltip.disabled;`;
 						}).join('')
 					+ `'`
@@ -193,7 +193,7 @@ function resolvChart(data, options) {
 
 
 		// ***************************************************************************************************** //
-		// ** Variavel Chart * //
+		// ** Variável Chart * //
 		+t(0)	+ 	`<script>`
 		+t(1)	+ 		`var chart${options.descForm} = null;`
 		+t(1)	+ 		`chart${options.descForm} = am4core.create("chartdiv${options.descForm}", am4charts.XYChart);`
@@ -236,7 +236,7 @@ function resolvChart(data, options) {
 		// ***************************************************************************************************** //
 		// ** Categoria * //
 		+ t()
-		+ (options.orientation == 'horizontal' 
+		+ (options.orientation == 'horizontal'
 			? t(1) + 	`categoryAxis${options.descForm} = chart${options.descForm}.xAxes.push(new am4charts.CategoryAxis());`
 			: t(1) + 	`categoryAxis${options.descForm} = chart${options.descForm}.yAxes.push(new am4charts.CategoryAxis());`
 		)
@@ -244,7 +244,7 @@ function resolvChart(data, options) {
 		+t(1)	+ 		`categoryAxis${options.descForm}.title.text 								= "${(options.category.text || '')}";`
 		+t(1)	+ 		`categoryAxis${options.descForm}.renderer.grid.template.location 			= 0;`
 		+t(1)	+ 		`categoryAxis${options.descForm}.renderer.minGridDistance 					= 20;`
-		+ (((options.category || {}).rotation || '') == '' ? '' : ''	
+		+ (((options.category || {}).rotation || '') == '' ? '' : ''
 			+t(1) +		`categoryAxis${options.descForm}.renderer.labels.template.rotation = ${options.category.rotation};`
 			// +t(1) +		`categoryAxis${options.descForm}.renderer.labels.template.horizontalCenter = "left";`
 			// +t(1) +		`categoryAxis${options.descForm}.renderer.labels.template.location = 0.5;`
@@ -284,7 +284,7 @@ function resolvChart(data, options) {
 		+ t()
 		+ (options.value || []).map(function(value={}, indiceVlr) { return ''
 			+ t(1) + 'var '
-			+ (options.orientation == 'horizontal' 
+			+ (options.orientation == 'horizontal'
 				? 			`valueAxis${options.descForm + String(indiceVlr)} = chart${options.descForm}.yAxes.push(new am4charts.ValueAxis());`
 				: 			`valueAxis${options.descForm + String(indiceVlr)} = chart${options.descForm}.xAxes.push(new am4charts.ValueAxis());`
 			)
@@ -323,21 +323,21 @@ function resolvChart(data, options) {
 	// ***************************************************************************************************** //
 	// ** Series * //
 	var maskLabel, color;
-	for (var i = 0; i < options.serie.length; i++) { 
+	for (var i = 0; i < options.serie.length; i++) {
 		if (((options.serie[i] || {}).name || '') != '') setLegend = true; // verifica se precisa de legenda
 
 		maskLabel = ((options.serie[i].label || {}).mask || '%0%').replace('%0%', (options.orientation == 'horizontal' ? '{valueY}' : '{valueX}'));
 
-		color = (options.serie[i].color || '') != '' 
+		color = (options.serie[i].color || '') != ''
 			? `am4core.color(\"${options.serie[i].color}\");`
 			: `chart${options.descForm}.colors.getIndex(${i});`;
 
 		html += ""+t()+t();
 
-		if ((options.serie[i].type || 'column') == 'column') { 
+		if ((options.serie[i].type || 'column') == 'column') {
 			html += ""
 			+t(1)	+ 		`series${i+options.descForm} = chart${options.descForm}.series.push(new am4charts.ColumnSeries());`
-			+ (options.orientation == 'horizontal' 
+			+ (options.orientation == 'horizontal'
 				? t(1) + 	`series${i+options.descForm}.dataFields.valueY 						= "${options.serie[i].param}";`
 				+ t(1) + 	`series${i+options.descForm}.dataFields.categoryX 					= "${options.category.param}";`
 				: t(1) + 	`series${i+options.descForm}.dataFields.valueX 						= "${options.serie[i].param}";`
@@ -346,7 +346,7 @@ function resolvChart(data, options) {
 			+t(1)	+ 		`series${i+options.descForm}.columns.template.fill 					= ${color};`
 			+t(1)	+ 		`series${i+options.descForm}.stroke 								= ${color};`
 			+t(1)	+ 		`series${i+options.descForm}.fill 									= ${color};`
-			+ (options.orientation == 'horizontal' 
+			+ (options.orientation == 'horizontal'
 				? t(1)	+ 		`series${i+options.descForm}.tooltip.dy 						= -8;`
 				: t(1)	+ 		`series${i+options.descForm}.tooltip.dx 						= -8;`
 			)
@@ -376,10 +376,10 @@ function resolvChart(data, options) {
 
 
 
-		if ((options.serie[i].type || 'column') == 'line') { 
+		if ((options.serie[i].type || 'column') == 'line') {
 			html += ""
 			+t(1)	+ 		`series${i+options.descForm} = chart${options.descForm}.series.push(new am4charts.LineSeries());`
-			+ (options.orientation == 'horizontal' 
+			+ (options.orientation == 'horizontal'
 				? t(1) + 	`series${i+options.descForm}.dataFields.valueY 				= "${options.serie[i].param}";`
 				+ t(1) + 	`series${i+options.descForm}.dataFields.categoryX 			= "${options.category.param}";`
 				: t(1) + 	`series${i+options.descForm}.dataFields.valueX 				= "${options.serie[i].param}";`
@@ -390,7 +390,7 @@ function resolvChart(data, options) {
 			+t(1)	+ 		`series${i+options.descForm}.stroke 						= ${color};` // set color line
 			+t(1)	+ 		`series${i+options.descForm}.fill 							= ${color};`
 			// +t(1)	+ 		"series"+i+options.descForm+".fillOpacity 					= 0.5;" // com fundo colorido ou não
-			+t(1)	+ 		`series${i+options.descForm}.strokeWidth 					= 3;` // espeçura da linha
+			+t(1)	+ 		`series${i+options.descForm}.strokeWidth 					= 3;` // espessura da linha
 			// chart.colors.getIndex(2);
 			+ ((options.serie[i].tooltip || '') == '' ? '' : ''
 				+t(1) + 	`series${i+options.descForm}.tooltipHTML 					= "${options.serie[i].tooltip}";`
@@ -425,7 +425,7 @@ function resolvChart(data, options) {
 			+t(1)	+ 		`series${i+options.descForm}.${((options.serie[i].hide || '') == '' ? 'show' : 'hide')}();`
 
 
-		if (options.serie[i].label != false) { 
+		if (options.serie[i].label != false) {
 			html += ''+t()
 			+t(1)	+ 		`var labelBullet${i+options.descForm} 					= new am4charts.LabelBullet();`
 			+t(1) 	+ 		`labelBullet${i+options.descForm}.label.text 			= "${maskLabel}";` // .value.formatNumber('#.')
@@ -433,19 +433,19 @@ function resolvChart(data, options) {
 			+t(1)	+ 		`labelBullet${i+options.descForm}.stroke 				= am4core.color("#dadada");`
 			+t(1)	+ 		`labelBullet${i+options.descForm}.label.strokeWidth 	= 0;`
 			+t(1)	+ 		`labelBullet${i+options.descForm}.label.fill 			= am4core.color("${((options.serie[i].label || {}).color || "black")}");`
-			+t(1)	+ 		`labelBullet${i+options.descForm}.label.hideOversized 	= true;` // não imprimi valor zerad
+			+t(1)	+ 		`labelBullet${i+options.descForm}.label.hideOversized 	= true;` // não imprimi valor zerado
 			+ ((options.serie[i].click || '') == '' ? '' : ''
 				+t(1) + 	`labelBullet${i+options.descForm}.events.on(\"hit\", function(ev) {`
 				+t(2) + 		`(${String(options.serie[i].click)}(ev));`
 				+t(1) + 	`});`
 			)
-			+ (((options.serie[i].label || {}).align || '').toLowerCase() == 'center' 
-				? (options.orientation == 'horizontal' 
+			+ (((options.serie[i].label || {}).align || '').toLowerCase() == 'center'
+				? (options.orientation == 'horizontal'
 					? t(1) + 	`labelBullet${i+options.descForm}.locationY 		= 0.5;`
 					: t(1) + 	`labelBullet${i+options.descForm}.locationX 		= 0.5;`
 				)
-				: (((options.serie[i].label || {}).align || '').toLowerCase() == 'in' 
-					? (options.orientation == 'horizontal' 
+				: (((options.serie[i].label || {}).align || '').toLowerCase() == 'in'
+					? (options.orientation == 'horizontal'
 						? t(1) + `labelBullet${i+options.descForm}.dy 				= 15;`
 						: t(1) + `labelBullet${i+options.descForm}.dx 				= -15;`
 					)
@@ -472,15 +472,15 @@ function resolvChart(data, options) {
 	html += ''+t()+t()
 		// ***************************************************************************************************** //
 		// ** Reta final * //
-		// +t(1)	+ 		"chart"+options.descForm+".legend 									= new am4charts.Legend(); // acrecenta legenda no grafico"
+		// +t(1)	+ 		"chart"+options.descForm+".legend 									= new am4charts.Legend(); // acrescenta legenda no grafico"
 		+t(1)	+ 		`chart${options.descForm}.scrollbarY 								= new am4core.Scrollbar();`
 		+t(1)	+ 		`var scrollbarX${options.descForm} 									= new am4core.Scrollbar();`
 		+t(1)	+ 		`chart${options.descForm}.scrollbarX 								= scrollbarX${options.descForm};`
 		+t(1)	+ 		`chart${options.descForm}.scrollbarX.parent 						= chart${options.descForm}.bottomAxesContainer;` // setar scroll do eixo x embaixo
 		// +t(1)	+ 		`chart${options.descForm}.scrollbarX.thumb.minWidth 				= 100;` // Limita o zoom do gráfico (serve pra nada kkkkk)
 
-		+t(1)	+ 		`chart${options.descForm}.cursor 									= new am4charts.XYCursor();` // indica onde o curso está com cordenadas XY
-		+ (options.orientation == 'horizontal' 
+		+t(1)	+ 		`chart${options.descForm}.cursor 									= new am4charts.XYCursor();` // indica onde o curso está com coordenadas XY
+		+ (options.orientation == 'horizontal'
 			? t(1) + 	`chart${options.descForm}.cursor.behavior 							= "zoomX";`
 			: t(1) + 	`chart${options.descForm}.cursor.behavior 							= "zoomY";`
 		)
@@ -504,7 +504,7 @@ function resolvChart(data, options) {
 		)
 		+ (!setLegend && (options.legend || '') == '' ? '' : ''
 			+ t(1) + 	`chart${options.descForm}.legend 			= new am4charts.Legend();`
-			+ ((options.legend || '') == '' ? '' 
+			+ ((options.legend || '') == '' ? ''
 				: ''
 				+ t(1) + `chart${options.descForm}.legend.position 	= "${(options.legend.position 	|| 'bottom'	)}";`
 				+ t(1) + `chart${options.descForm}.legend.valign 	= "${(options.legend.align 		|| 'top'	)}";`
@@ -516,12 +516,12 @@ function resolvChart(data, options) {
 	return html;
 }
 
-function toFunction(func, replace) { 
+function toFunction(func, replace) {
 	var isString;
 	func = String(func);
 	for (var i = 0; i < replace.length; i++) {
 		isString = (replace[i][2] || 'literal') == 'literal' && typeof(replace[i][1]) == 'string';
-		while (func.indexOf(replace[i][0]) != -1) 
+		while (func.indexOf(replace[i][0]) != -1)
 			func = func.replace(replace[i][0] , isString ? "\"" + replace[i][1] + "\"" : replace[i][1]);
 	}
 	return eval(`(function(){ return ${func}})()`);
