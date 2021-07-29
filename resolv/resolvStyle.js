bdb4ef5928b8bd5cb6449f11b3859f87c1be2130
jackbiller19 <jackbiller19@gmail.com>
2021-07-28 23:14:16 -0300

    Update resolvStyle.js

--- resolv/resolvStyle.js
+++ resolv/resolvStyle.js
@@ -1,12 +1,7 @@
 
 function resolvStyle(obj) { 
-	// var html = '';
+	if (typeof obj == 'string') return obj;
+
 	var keys = Object.keys(obj);
-
-	// return keys.map(k => k + ':' + obj[k] + ';').join('');
 	return keys.map(function(k) { return k + ':' + obj[k] + ';'; }).join('');
-
-	// for (var i = 0; i < keys.length; i++)
-	// 	html += keys[i] + ':' + obj[keys[i]] + ';';
-	// return html;
 }
